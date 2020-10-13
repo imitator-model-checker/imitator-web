218349ed5d569c56361861b7a968dcbdc6a0089f
Jaime Arias Almeida <arias@lipn.univ-paris13.fr>
2020-10-13 11:16:41 +0200

    fix: number of days for deleting temps files is a paramter

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@ module.exports = {
   uploadFolder: process.env.UPLOAD_FOLDER || '/tmp/imitator-runner',
   imitatorMode: process.env.IMITATOR_MODE || 'binary',
   imitatorPath: process.env.IMITATOR_PATH || 'imitator',
+  timeLimit: process.env.TIME_LIMIT_FILES || '7', // in days
   port: process.env.PORT || '3000',
 };
