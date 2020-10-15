a8b571d22e617306fae9732fe6d0056e0e9daf92
root <root@cosyverif.lipn.univ-paris13.fr>
2020-10-15 09:36:07 +0200

    remove generate_BFS script

--- config/artifact.js
+++ config/artifact.js
@@ -2,7 +2,7 @@ module.exports = {
   artifacts: {
     tacas21: {
       image: 'imitator:tacas21',
-      scripts: ['brp_experiments', 'generate_BFS', 'generate_iterative', 'generate_strategies'],
+      scripts: ['brp_experiments', 'generate_iterative', 'generate_strategies'],
       output_arg: '-o',
     },
   },
